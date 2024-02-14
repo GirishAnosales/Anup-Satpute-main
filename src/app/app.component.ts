cb63c0a30965b93004ff1a9382225c50682b8749
Morish-Anosales <anosales207@gmail.com>
2024-02-14 16:43:47 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+declare var gtag:any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Anup-Satpute';
+
+  constructor(private router: Router){
+		// const navEndEvents = router.events.pipe(
+		//   filter(event => event instanceof NavigationEnd),
+		// );
+		// navEndEvents.subscribe((event: NavigationEnd) => {
+		//   gtag('config', 'AW-11454875150', {
+		//     'page-path': event.urlAfterRedirects
+		//   });
+		// })
+	
+		  const routeChange = router.events
+			.pipe(
+			  filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+			)
+			.subscribe(event => {
+			  gtag('config', 'G-KMQ5XQ76Q7', {
+				'page-path': event.urlAfterRedirects
+			  })
+			});
+		
+	  }
+
 }
